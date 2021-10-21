e35074c2f64afd7a6fecd9180cf267f442cc48cc
Maria Pezzolo <mariaolgapezzolo@gmail.com>
2021-10-21 15:28:33 -0300

    ejer

--- prueba.js
+++ prueba.js
@@ -1,2 +1,27 @@
-console.log('Hola mundo')
-console.log('Hola mundo')
+//console.log('Hola mundo')
+//console.log('Hola mundo')
+let persona = {
+    nombre: 'luisa',
+    telefono: '319565776'
+  }
+  
+  let persona2 = {
+    nombre: 'martin',
+    telefono: '3195657762'
+  }
+let agendaSoloNumeros = [1, 2, 3, 4, 5]
+let agendaConVariosTipos = [1, true, false, 'carlos', persona]
+console.log(agendaSoloNumeros, agendaConVariosTipos)
+let persona_nueva = {
+    altura: 1.70,
+    edad: 28,
+    nombre: 'Pepito'
+  
+  }
+  persona_nueva.cabello = 'negro'
+  // altura = atributo
+  // 1.70 = valor
+  
+  console.log(typeof(persona_nueva))
+  console.log(persona_nueva)
+  
