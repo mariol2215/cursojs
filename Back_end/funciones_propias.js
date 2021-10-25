55087b4da5163749c6fe8d50d74c6837f8f84e86
Maria Pezzolo <mariaolgapezzolo@gmail.com>
2021-10-25 20:52:50 -0300

    inicio

---
+++ Back_end/funciones_propias.js
@@ -0,0 +1,24 @@
+
+function sumar (a,b) {
+    return a + b
+}
+function restar (a,b) {
+    return a - b
+}
+function multiplicar (a,b) {
+    return a * b
+}
+function dividir (a,b) {
+    return a / b
+}
+
+
+module.exports = {
+
+    Suma: sumar,
+    Resta: restar,
+    Multiplica: multiplicar,
+    Divide: dividir
+
+}
+
---
+++ Back_end/index.js
@@ -0,0 +1,7 @@
+const funciones_propias = require('./funciones_propias')
+const calculadora = require('./funciones_propias')
+
+console.log(" la suma de 2 y 4 es", calculadora.Suma(2,4))
+console.log(" la resta de 2 y 4 es", calculadora.Resta(2,4))
+console.log(" la multiplicación de 2 y 4 es", calculadora.Multiplica(2,4))
+console.log(" la división de 2 y 4 es", calculadora.Divide(2,4))
